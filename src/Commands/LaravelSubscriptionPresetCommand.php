bd37b29201db58917cf65d612bf8496ce42f46eb
Morten Bak <hej@netbums.dk>
2022-07-07 20:18:32 +0200

    add install method/command

--- src/Commands/LaravelSubscriptionPresetCommand.php
+++ src/Commands/LaravelSubscriptionPresetCommand.php
@@ -3,15 +3,18 @@
 namespace Netbums\LaravelSubscriptionPreset\Commands;
 
 use Illuminate\Console\Command;
+use Netbums\LaravelSubscriptionPreset\LaravelSubscriptionPreset;
 
 class LaravelSubscriptionPresetCommand extends Command
 {
     public $signature = 'laravel-subscription-preset';
 
-    public $description = 'My command';
+    public $description = 'Install the presets for subscriptions using stripe/cashier';
 
     public function handle(): int
     {
+        LaravelSubscriptionPreset::install();
+
         $this->comment('All done');
 
         return self::SUCCESS;
--- src/LaravelSubscriptionPreset.php
+++ src/LaravelSubscriptionPreset.php
@@ -2,6 +2,16 @@
 
 namespace Netbums\LaravelSubscriptionPreset;
 
+use Illuminate\Filesystem\Filesystem;
+
 class LaravelSubscriptionPreset
 {
+
+
+    public static function install()
+    {
+        $filesystem = new Filesystem();
+        $filesystem->copyDirectory(__DIR__ . '/../stubs/default', base_path());
+
+    }
 }
