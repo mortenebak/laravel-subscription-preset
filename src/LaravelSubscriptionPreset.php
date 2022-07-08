be2f5d76c468da90e2211e9a56519aea45e32285
Morten Bak <meb@indexed.dk>
2022-07-08 15:12:27 +0200

    add: todo note

--- src/LaravelSubscriptionPreset.php
+++ src/LaravelSubscriptionPreset.php
@@ -10,5 +10,8 @@ class LaravelSubscriptionPreset
     {
         $filesystem = new Filesystem();
         $filesystem->copyDirectory(__DIR__ . '/../stubs/default', base_path());
+
+        // Todo: Auto insert env vars instead of copying over the .env.example and asking users to insert vars manually
+
     }
 }
