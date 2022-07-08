e9f6eff371b7cdf0898b5a3149c144826d7199d4
mortenebak <mortenebak@users.noreply.github.com>
2022-07-08 13:13:03 +0000

    Fix styling

--- src/LaravelSubscriptionPreset.php
+++ src/LaravelSubscriptionPreset.php
@@ -12,6 +12,5 @@ class LaravelSubscriptionPreset
         $filesystem->copyDirectory(__DIR__ . '/../stubs/default', base_path());
 
         // Todo: Auto insert env vars instead of copying over the .env.example and asking users to insert vars manually
-
     }
 }
